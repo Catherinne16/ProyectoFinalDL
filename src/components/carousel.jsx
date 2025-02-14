fc60514a158aba2e4313bf7f1bff798eebd971e7
pipeboxes <felipe.cajas.maureira@gmail.com>
2025-02-14 00:14:17 -0300

    Modificaciones Carousel y Navbar

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,22 +1,37 @@
 // src/components/Carousel.jsx
 import { useState, useEffect } from "react";
-import banner1 from "../assets/images/banner1.jpg"; // Importa la imagen aquí
+import banner2 from "../assets/images/banner2.jpg";
+import banner3 from "../assets/images/banner3.jpg";
+import banner4 from "../assets/images/banner4.jpg";
+import banner5 from "../assets/images/banner5.jpg";
+
+
 import "./carousel.css";
 
 const Carousel = () => {
-  const images = [banner1];
+  const images = [banner2, banner3, banner4, banner5];
   const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 3000);
+      nextImage();
+    }, 5000);
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, [currentImage]);
+
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
 
   return (
     <div className="carousel">
+      <button className="prev" onClick={prevImage}>❮</button>
       <img src={images[currentImage]} alt="banner" />
+      <button className="next" onClick={nextImage}>❯</button>
     </div>
   );
 };
